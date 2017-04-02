f8d9f4c2596a0112e4a6a17b1716942c1ec697b0
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-04-02 16:54:19 -0400

    addition of print statements for s.peek() to show current top of stack

--- Main.java
+++ Main.java
@@ -7,8 +7,9 @@ public class Main
     s.push(5);
     s.push(7);
     s.push(2);
-    s.peek();
+    System.out.println(s.peek() + " is the current top of the stack");
     s.pop();
     s.pop();
+    System.out.println(s.peek() + " is the current top of the stack");
   }
 }
--- Stack.java
+++ Stack.java
@@ -16,13 +16,11 @@ public class Stack
   public int peek()
   {
     int x = stack[top - 1];
-    // System.out.println(x + " is the top");
     return x;
   }
   public int pop()
   {
     int y = stack[top - 1];
-    // System.out.println(y + " is the top");
     top--;
     return y;
   }
