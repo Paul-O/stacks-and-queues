b4c11f096fd77c4805826715e9eaf077a134a73b
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-04-02 16:26:14 -0400

    finished push method

--- Stack.java
+++ Stack.java
@@ -1,11 +1,16 @@
 public class Stack
 {
-  int top = -1;
+  int top = 0;
   int[] stack;
   int maxSize;
   public Stack(maxSize)
   {
     stack = new int[maxSize];
     this.maxSize = maxSize;
+  }
+  public void push(int a) // method takes in a parameter
+  {
+    stack[top] = a;
+    top++;
   }
 }
