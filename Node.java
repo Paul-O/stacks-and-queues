208803a7826133bd7323cbf905183b92d0c6b6c3
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-04-07 14:20:16 -0400

    made node

---
+++ Node.java
@@ -0,0 +1,25 @@
+public class Node
+{
+  int data;
+  Node next;
+  public Node(int data)
+  {
+    this.data = data;
+  }
+  public void setData(int d)
+  {
+    this.data = d;
+  }
+  public int getData()
+  {
+    return data;
+  }
+  public void setNext(Node n)
+  {
+    this.next = n;
+  }
+  public Node getNext()
+  {
+    return next;
+  }
+}
