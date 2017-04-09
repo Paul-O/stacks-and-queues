b8e0c586b64488afeab33ad6d56f491c7ce0f9d0
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-04-09 17:31:34 -0400

    Step 1 of Main method (stack to queue) done

--- Main.java
+++ Main.java
@@ -2,6 +2,17 @@ public class Main
 {
   public static void main(String[] args)
   {
-    
+    Stack s = new Stack();
+    s.push(12);
+    s.push(34);
+    s.push(51);
+    s.print();
+    System.out.println(" ");
+    Queue q = new Queue();
+    for(int c = 1; c < 4; c++)
+    {
+      q.enqueue(s.pop());
+    }
+    q.print();
   }
 }
