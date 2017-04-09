abb80ea684d5fa762789b944754178202db4bcb1
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-04-09 17:22:27 -0400

    voided main method

--- Main.java
+++ Main.java
@@ -2,18 +2,6 @@ public class Main
 {
   public static void main(String[] args)
   {
-    /* Stack s = new Stack();
-    s.push(3);
-    s.push(34);
-    s.push(7);
-    s.push(113);
-    s.push(5);
-    s.print(); */
     
-    Queue q = new Queue();
-    q.enqueue(2);
-    q.enqueue(3);
-    q.enqueue(24);
-    q.print();
   }
 }
